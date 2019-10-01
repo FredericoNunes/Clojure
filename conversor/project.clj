2eb97e4648d1bbac5eb9612ab5b321e45d62c31b
frederico <fredericonunes@outlook.com>
2019-10-01 01:02:56 -0300

    CHAVE_API=apiKey lein run --de=USD --para=BRL

--- conversor/project.clj
+++ conversor/project.clj
@@ -4,7 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/tools.cli "0.4.1"]]
+                 [clj-http "3.9.1"]
+                 [org.clojure/tools.cli "0.4.1"]
+                 [cheshire "5.8.1"]]
   :main ^:skip-aot conversor.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- conversor/src/conversor/core.clj
+++ conversor/src/conversor/core.clj
@@ -1,5 +1,7 @@
 (ns conversor.core
-  (:require [clojure.tools.cli :refer [parse-opts]])
+  (:require [clojure.tools.cli :refer [parse-opts]]
+            [clj-http.client :as http-client]
+            [cheshire.core :refer [parse-string]])
   (:gen-class))
 
 
@@ -7,8 +9,32 @@
   [["-d" "--de moeda base" "moeda base para conversão" :default "eur"]
    ["-p" "--para moeda destino" "moeda a qual queremos saber o valor"]])
 
+(def chave (System/getenv "CHAVE_API"))
+
+(def api-url 
+    "https://free.currconv.com/api/v7/convert")
+
+
+(defn parametrizar-moedas [de para]
+  (str de "_" para))
+
+
+(defn obter-cotacao [de para] 
+  (let [moedas (parametrizar-moedas de para)]
+    (-> (:body (http-client/get api-url
+                                         {:query-params {"q" moedas
+                                                         "apiKey" chave}}))
+        (parse-string)
+        (get-in ["results" moedas "val"]))))
+
+
+(defn- formatar [cotacao de para]
+  (str "1 " de " equivale a " cotacao " em " para))
+
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (prn "as opcoes são:" (:options 
-                            (parse-opts args opcoes-do-programa))))
+  (let [{:keys [de para]} (:options (parse-opts args opcoes-do-programa))]
+        (-> (obter-cotacao de para)
+            (formatar de para)
+            (prn))))
